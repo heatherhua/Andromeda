c53e4d7c29e26d78f70e3d78a38956b1f319d618
Heather Hua <hhua729@gmail.com>
2014-07-12 00:15:25 -0700

    Submit button works to go to another page

--- Andromeda/Andromeda/src/com/example/andromeda/MainActivity.java
+++ Andromeda/Andromeda/src/com/example/andromeda/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.andromeda;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.example.andromeda.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,15 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	/* Called when user clicks submit question button */
+	public void sendQuestion(View view){
+		Intent intent = new Intent(this, AnswerQuestionActivity.class);
+		EditText editText = (EditText) findViewById(R.id.user_question);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
+
 	}
 
 }
